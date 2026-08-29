1627a193e8a3de0c75a1501a7f00bd14fb4bbbd0
AbhilashGUI <vemulaabhilash8433@gmail.com>
2026-08-29 17:24:14 +0530

    Updated code

--- src/test/java/SeleniumPB/SeleniumP18.java
+++ src/test/java/SeleniumPB/SeleniumP18.java
@@ -1,4 +1,4 @@
-package SeleniumPB;
+package  SeleniumPB;
 
 import io.qameta.allure.Description;
 import org.openqa.selenium.By;
@@ -6,46 +6,48 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-
-
+import java.util.List;
 
 public class SeleniumP18 {
 
     EdgeDriver driver;
+
     @BeforeTest
     public void openbrowser()
     {
-        EdgeOptions options= new EdgeOptions();
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        options.addArguments("--guest");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-infobars");
-        driver=new EdgeDriver(options);
+        EdgeOptions edgeOptions=new EdgeOptions();
+        edgeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        edgeOptions.addArguments("--guest");
+        edgeOptions.addArguments("--disable-notifications");
+        edgeOptions.addArguments("--disable-extensions");
+        edgeOptions.addArguments("--disable-infobars");
+        driver=new EdgeDriver(edgeOptions);
     }
 
     @Test(groups = "QA")
-    @Description("Verify checkboxes")
-    public void checkbox()
-    {
-
-        driver.get("https://demoqa.com/checkbox");
+    @Description("Verify the checkboxes")
+    public void checkboxes() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
         driver.manage().window().maximize();
-        //<svg
-        //stroke="currentColor"
-        //fill="currentColor"
-        //stroke-width="0"
-        //viewBox="0 0 24 24"
-        //class="rct-icon rct-icon-uncheck"
-        // height="1em"
-        // width="1em"
-        // xmlns="http://www.w3.org/2000/svg">
-        // <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"></path></svg>
 
-        WebElement checkbox= driver.findElement(By.tagName("svg"));
-        checkbox.click();
-        driver.quit();
+        //<input type="checkbox" checked="">  checkbox 1
+        //<input type="checkbox" checked="">  checkbox 2
+
+
+        List<WebElement> checkboxes=driver.findElements(By.xpath("//input[@type='checkbox']"));
+        checkboxes.get(0).click();
+        checkboxes.get(1).click();
+        Thread.sleep(3000);
+    }
+
+    @AfterTest
+    public void closebrowser()
+    {
+        if(driver !=null)
+            driver.quit();
     }
 }
